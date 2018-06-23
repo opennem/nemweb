1a493de36481d440c155f35aa2353b005e852013
Dylan McConnell <dylan@localhost>
2018-06-24 00:51:01 +1000

    changed nemfile class to 'nemfile_reader' function. Added doc strings and linted

--- nemfile_reader.py
+++ nemfile_reader.py
@@ -1,57 +1,58 @@
-#python3
+"""Module for reading nemfiles and zipped nemfiles into pandas dataframes"""
+
+from io import BytesIO
+from zipfile import ZipFile
 import pandas as pd
-from io import StringIO,BytesIO
-from zipfile import ZipFile, BadZipfile
 
 class ZipFileStreamer(ZipFile):
-	"""ZipFile subclass, with method to extract ZipFile as byte or string stream to memory"""
+    """ZipFile subclass, with method to extract ZipFile as byte stream to memory"""
 
-	def __init__(self,filename = '/data/marble/nemweb/ARCHIVE/DispatchIS_Reports/PUBLIC_DISPATCHIS_20170208.zip'):
-		"""Open the ZIP file with mode read "r", write "w" or append "a" """
-		ZipFile.__init__(self, filename)
-		self.member_count = len(self.filelist)
-		
-	def extract_stream(self,member, IO = BytesIO):
-		"""	Extract a member from the archive as a byte stream or string 
-		steam, using its full name. `member' may be a filename or a 
-		ZipInfo object. """
-		return IO(self.read(member))
+    def __init__(self, filename):
+        """Initialises ZipFile object, and adds member_count attribute"""
+        ZipFile.__init__(self, filename)
+        self.member_count = len(self.filelist)
 
-def zip_streams(fileobject='/data/marble/nemweb/ARCHIVE/DispatchIS_Reports/PUBLIC_DISPATCHIS_20140208.zip'):
-	"""Generator that yields each memeber of a zipfile as a BytesIO stream.
-	Can take a filename or file-like object as an argument (including BytesIO or StringIO"""
-	with ZipFileStreamer(fileobject) as zf:
-		for filename in zf.namelist():
-			yield filename,zf.extract_stream(filename)
+    def extract_stream(self, member):
+        """Extract a member from the archive as a byte stream or string steam, using
+        its full name. `member' may be a filename or a ZipInfo object. """
+        return BytesIO(self.read(member))
 
-class NemFile():
-	def __init__(self,fileobject):
-		self.__table_reader(fileobject)
+def zip_streams(fileobject):
+    """Generator that yields each memeber of a zipfile as a BytesIO stream.
+    Can take a filename or file-like object (BytesIO object) as an argument."""
+    with ZipFileStreamer(fileobject) as zipfile:
+        for filename in zipfile.namelist():
+            yield filename, zipfile.extract_stream(filename)
 
-	def __table_reader(self,fileobject):
-		for line in fileobject.readlines():
-			rows = line.decode().split(',')
-			table = "{0}_{1}".format(rows[1],rows[2])
-			
-			#new table 
-			if rows[0] == "I":
-				self.__dict__[table] = line
+def nemfile_reader(nemfile_object):
+    """Returns a dict containing a pandas dataframe each table in a nemfile.
+    The fileobject needs to be unzipped csv (nemfile), and can be either a file or an
+    an in stream fileobject."""
+    table_dict = {}
+    for line in nemfile_object.readlines():
+        rows = line.decode().split(',')
+        table = "{0}_{1}".format(rows[1], rows[2])
 
-			#append data to each table
-			elif rows[0] == "D":
-				self.__dict__[table] += line
-	
-		self.__dict__ = {table:pd.read_csv(BytesIO(self.__dict__[table])) for table in self.__dict__}	
+        #new table
+        if rows[0] == "I":
+            table_dict[table] = line
 
-	def __iter__(self):
-		for table in self.__dict__:
-			yield table
+        #append data to each table
+        elif rows[0] == "D":
+            table_dict[table] += line
 
-def load_nemfile(f="PUBLIC_DISPATCHIS_201309150010_0000000246668929.zip"):
-	with ZipFileStreamer(f) as zf:
-		if zf.member_count == 1:
-			filename = zf.namelist()[0]	
-			zfs = zf.extract_stream(filename)
-			return NemFile(zfs)			
-		else:
-			raise Exception('More than one file in zipfile')
+    return {table:pd.read_csv(BytesIO(table_dict[table])) for table in table_dict}
+
+def nemzip_reader(nemzip_object):
+    """Returns a dict containing a pandas dataframe each table in a zipped nemfile.
+    The fileobject is needs to be a zipped csv (nemzip), and can be either a file or an
+    in stream fileobject.
+    Function checks there is only one file to unzip, unzips to a nemfile (csv) in memory,
+    and passes nemfile_object to nemfile reader."""
+    with ZipFileStreamer(nemzip_object) as zipfile:
+        if zipfile.member_count == 1:
+            filename = zipfile.namelist()[0]
+            nemfile_object = zipfile.extract_stream(filename)
+            return nemfile_reader(nemfile_object)
+        else:
+            raise Exception('More than one file in zipfile')
