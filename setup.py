db3617ee6008b81bd65dc23caadc0ac8cd16b1ad
Dylan McConnell <dylan@localhost>
2018-06-24 23:26:27 +1000

    Added post-install and post-develop scripts

--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
+import os
+import configparser
 from setuptools import setup
+from setuptools.command.install import install
+
+def create_config():
+    local_dir = os.path.expanduser("~")
+    config = configparser.ConfigParser()
+    config.add_section("local_settings")
+    _dir = input("Enter directory (abs path) to store for sqlite db:")
+    config.set("local_settings","sqlite_dir",_dir)
+    with open(os.path.join(local_dir,".nemweb_config.ini"), "w") as cfgfile:
+        config.write(cfgfile)
+
+
+class PostInstallCommand(install):
+    """Post-installation for install mode."""
+    def run(self):
+        create_config()
+        install.run(self)
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        create_config()
+        develop.run(self)
 
 setup(name='nemweb',
       version='0.1',
@@ -11,4 +36,9 @@ setup(name='nemweb',
       zip_safe=False,
       install_requires=[
           'pandas',
-          'requests'])
+          'requests'],
+      cmdclass={'install': PostInstallCommand},
+                'develop': PostDevelopCommand})
+
+
+
