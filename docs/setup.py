2896ba6830ca6443a489ee2c7bfc77813d3e5fa3
Dylan McConnell <dylan@localhost>
2018-06-24 18:46:28 +1000

    Moved setup to docs

---
+++ docs/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='nemweb',
+      version='0.1',
+      description="newweb file handler",
+      long_description="",
+      author='dylan',
+      author_email='dylan@opennem.org.au',
+      license='MIT',
+      packages=['nemweb'],
+      zip_safe=False,
+      install_requires=[
+          'pandas',
+          'requests'])
