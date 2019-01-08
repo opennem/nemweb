3b43b209a284d0b3b119d5bd66455ed422b1dba3
Dylan McConnell <dylan.j.mcconnell@gmail.com>
2019-01-08 15:59:10 +1100

    Removed old test data, modified test to download latest trading interval data, and checks three tables are made. (Doesn't check data is actually inserted in tables, yet)

--- nemweb/tests/test_nemweb.py
+++ nemweb/tests/test_nemweb.py
@@ -3,13 +3,16 @@
 import os
 import pkg_resources
 import sqlite3
+import datetime
+import pytest
 
 from nemweb import CONFIG
 from nemweb.nemweb_current import CurrentFileHandler, DATASETS, update_datasets
 
-from nemweb.utils import load_pickle
+from nemweb.utils import load_pickle, local_to_nem_tz
 
 
+#setup = perhaps this could be turned into a fixture?
 def test_nemweb():
     db_path = os.path.join(
         CONFIG['local_settings']['sqlite_dir'], 'test.db'
@@ -21,21 +24,32 @@ def test_nemweb():
 
     handler = CurrentFileHandler()
 
+    #test latest previous trading_interval
+    local_datetime = datetime.datetime.now()
+    nemtime = local_to_nem_tz(local_datetime)
+    start_datetime = nemtime - datetime.timedelta(0,1800)
+
     handler.update_data(
-        DATASETS['trading_is'], start_date='20180921', end_date='20180922',
-        print_progress=True, db_name='test.db'
+        DATASETS['trading_is'],
+        print_progress=True,
+        db_name='test.db',
+        start_date = start_datetime
     )
 
+test_nemweb()
+
+def test_table_creation():
     conn = sqlite3.connect(db_path)
     cur = conn.cursor()
+    test_data = cur.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
+    assert test_data == [('TRADING_PRICE',), ('TRADING_REGIONSUM',), ('TRADING_INTERCONNECTORRES',)]
 
-    test_data = cur.execute("SELECT RRP FROM TRADING_PRICE").fetchall()
+#@pytest.mark.parametrize("section", CONFIG.sections())
+def test_section_keys(table="TRADING_PRICE"):
+    """checks keys data in tables (and lenth data matches expected data in trading interval)"""
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
+    return (cur.execute("SELECT count(*) FROM ='{0}'".format(table).fetchall()
 
 
-    test_data_check = pkg_resources.resource_filename(
-        'nemweb', 'tests/2018_09_21.pkl'
-    )
 
-    test_data_check = load_pickle(test_data_check)
-
-    assert test_data == test_data_check
