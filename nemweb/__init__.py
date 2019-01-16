aa8765590bd4c7258e8b0aa55dc76ddeca2d2d65
Dylan McConnell <dylan@localhost>
2019-01-16 23:10:58 +1100

    hard code config location (for docker app)

--- nemweb/__init__.py
+++ nemweb/__init__.py
@@ -1,7 +1,4 @@
 """Initialises nemweb package, loads config"""
 import configparser
-import os
-
-CONFIG_DIR = "/nemweb/"
 CONFIG = configparser.RawConfigParser()
-CONFIG.read(os.path.join(CONFIG_DIR, 'config.ini'))
+CONFIG.read(os.path.join('/data/config.ini'))
